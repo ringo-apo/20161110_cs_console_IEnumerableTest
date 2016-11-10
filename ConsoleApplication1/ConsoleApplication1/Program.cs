e674ed16a59476a85050dc4e52503d4f9ab223d9
akira <ringo1625@gmail.com>
2016-11-10 13:37:15 +0900

    IEnumable化

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApplication1
+{
+    class IntArray : IEnumerable
+    {
+        private int[] arr;
+
+        public IntArray(int[] arr)
+        {
+            this.arr = arr;
+        }
+
+        // インデクサ
+        public int this[int index]
+        {
+            get { return arr[index]; }
+            set { arr[index] = value; }
+        }
+
+        // 長さ
+        public int Length
+        {
+            get { return arr.Length; }
+        }
+
+        // IEnumerable.GetEnumeratorの実装
+        public IEnumerator GetEnumerator()
+        {
+            // Array.GetEnumerator()の戻り値を流用する
+            return arr.GetEnumerator();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntArray arr = new IntArray(new int[] { 0, 1, 2, 3, 4 });
+
+            foreach (int _ in arr)
+            {
+                Console.WriteLine(_);
+            }
+            Console.ReadLine();
+        }
+    }
+}
